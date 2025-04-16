37b4a887afc5eae55e8fed6351b7e413b51a8af5
Marina Schneider <160941462+EscandioNeider@users.noreply.github.com>
2025-04-16 07:53:28 -0300

    Update Content.js

--- src/components/Content.js
+++ src/components/Content.js
@@ -6,7 +6,7 @@ const Content = () => {
       <h2>Bem-vindo à minha incrível página</h2>
       <p>
         Este é um desafio proposto pelo orientador: Quem desenvolver o site mais bonito e criativo, ganhará um prêmio!
-        Meu site, basicamente, trabalha com mudanças temáticas rápidas e simples. Teste cada uma delas!
+        Meu site, basicamente, trabalha com mudanças temáticas rápidas e simples. Teste cada uma delas! Ao acionar um botão, você deve esperar o tempo de exibição do tema antes de testar o próximo...
       </p>
     </div>
   );
